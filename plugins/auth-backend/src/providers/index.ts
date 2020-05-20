8235decddddc0a8a0ee64cbe039d20a4178287a2
Raghunandan <soapraj@gmail.com>
2020-05-20 11:27:35 +0200

    tests for makeProvider

--- plugins/auth-backend/src/providers/index.ts
+++ plugins/auth-backend/src/providers/index.ts
@@ -17,12 +17,6 @@
 import Router from 'express-promise-router';
 import { AuthProviderRouteHandlers, AuthProviderFactories } from './types';
 
-import { GoogleAuthProvider } from './google/provider';
-
-const providerFactories: AuthProviderFactories = {
-  google: GoogleAuthProvider,
-};
-
 export const defaultRouter = (provider: AuthProviderRouteHandlers) => {
   const router = Router();
   router.get('/start', provider.start);
@@ -34,11 +28,16 @@ export const defaultRouter = (provider: AuthProviderRouteHandlers) => {
   return router;
 };
 
-export const makeProvider = (config: any) => {
+export const makeProvider = (
+  providerFactories: AuthProviderFactories,
+  config: any,
+) => {
   const providerId = config.provider;
   const ProviderImpl = providerFactories[providerId];
   if (!ProviderImpl) {
-    throw Error(`Provider Implementation missing for provider: ${providerId}`);
+    throw Error(
+      `Provider Implementation missing for : ${providerId} auth provider`,
+    );
   }
   const providerInstance = new ProviderImpl(config);
   const strategy = providerInstance.strategy();
